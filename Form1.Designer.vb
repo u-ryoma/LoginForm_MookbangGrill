1e55e4b62c5800cdcc580ccfef1e80853c324a6e
Joshua <ryomaechizen1515@gmail.com>
2025-09-07 13:24:51 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        Label3 = New Label()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(191, 176)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(100, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Username: "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(191, 209)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(91, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Password:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(288, 170)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(201, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(288, 206)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(201, 31)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(271, 262)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 4
+        Button1.Text = "Login"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(504, 209)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(162, 29)
+        CheckBox1.TabIndex = 5
+        CheckBox1.Text = "Show Password"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 20F)
+        Label3.Location = New Point(253, 81)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(298, 54)
+        Label3.TabIndex = 6
+        Label3.Text = "Mookbang Grill"
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.IndianRed
+        Button2.Location = New Point(407, 262)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(107, 35)
+        Button2.TabIndex = 7
+        Button2.Text = "Cancel"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Label3)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        DoubleBuffered = True
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Login"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class Form1
+
+
+
+    Dim connectionString As String = "Data Source=.\SQLEXPRESS01;Initial Catalog=GrillMate;Integrated Security=True"
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        TextBox2.PasswordChar = "*"c
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.PasswordChar = ""
+        Else
+            TextBox2.PasswordChar = "*"c
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim username As String = TextBox1.Text.Trim()
+        Dim password As String = TextBox2.Text.Trim()
+
+        Dim connectionString As String = "Server=.\SQLEXPRESS01;Database=GrillMate;Trusted_Connection=True;"
+        Dim query As String = "SELECT COUNT(*) FROM fUser WHERE [User] = @Username AND [Password] = @Password"
+
+        Using conn As New SqlConnection(connectionString)
+            Using cmd As New SqlCommand(query, conn)
+
+                cmd.Parameters.AddWithValue("@Username", username)
+                cmd.Parameters.AddWithValue("@Password", password)
+
+                Try
+                    conn.Open()
+                    Dim result As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+
+                    If result > 0 Then
+                        MessageBox.Show("Login successful!", "Successful!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Me.Hide()
+                        Form2.Show()
+                    Else
+                        MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+                    End If
+
+                Catch ex As Exception
+                    MessageBox.Show("Database error: ", "ERROR", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+                End Try
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+End Class
